82b5ff924a12961c6c5c97a6c0d4039d66a86369
RektyRowdyy <rektyrowdy@gmail.com>
2024-09-23 20:45:56 +0530

    added portfolio + chess-clone

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,12 +3,13 @@ import MyProjects from "./MyProjects";
 import { DiVisualstudio } from "react-icons/di";
 import { TbFileTypeSql } from "react-icons/tb";
 import { FaNodeJs, FaReact } from "react-icons/fa";
-
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 
 export default function Projects() {
     return (
-        <section className="bg-violet-300 dark:bg-gray-700 h-[40rem]">
+        <section className="bg-violet-300 dark:bg-gray-700 h-[55rem]">
             {/* HEADING */}
             <div className="flex flex-col items-center">
                 <h1 className="text-2xl mt-24 font-semibold font-mono dark:text-white">PROJECTS</h1>
@@ -23,9 +24,15 @@ export default function Projects() {
                 <MyProjects title="YOUTUBE BACKEND" year="2024"
                     imgUrls={[`Youtube-Backend/1.png`, `Youtube-Backend/2.png`, `Youtube-Backend/3.png`]}
                     link="https://github.com/RektyRowdyy/Youtube-Backend" icons={[FaNodeJs, SiExpress, SiMongodb]} />
-                <MyProjects title="FreeCodeCamp-AUTH" year="2024"
+                <MyProjects title="FREECODECAMP-AUTH" year="2024"
                     imgUrls={[`FreeCodeCamp-AUTH/1.png`, `FreeCodeCamp-AUTH/2.png`, `FreeCodeCamp-AUTH/3.png`, `FreeCodeCamp-AUTH/4.png`]}
-                    link="https://github.com/RektyRowdyy/freecodecamp-AUTH" icons={[SiMongodb,SiExpress, FaReact, FaNodeJs]} />
+                    link="https://github.com/RektyRowdyy/freecodecamp-AUTH" icons={[SiMongodb, SiExpress, FaReact, FaNodeJs]} />
+                <MyProjects title="HRIDYA-PORTFOLIO" year="2024"
+                    imgUrls={[`Portfolio/1.png`,`Portfolio/2.png`,`Portfolio/3.png`,`Portfolio/4.png`,`Portfolio/5.png` ]}
+                    link="https://github.com/RektyRowdyy/Chess" icons={[FaReact, IoLogoJavascript, RiTailwindCssFill]} />
+                <MyProjects title="CHESS-CLONE (in Progress)" year="2024"
+                    imgUrls={[`https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTh1d21ldDQ0NnJ1djEzZTRzaGpvbWgwOGZrb3NxN2EyZ21sMzJlYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/zOvBKUUEERdNm/giphy.gif`]}
+                    link="https://github.com/RektyRowdyy/Chess" icons={[SiMongodb, SiExpress, FaReact, FaNodeJs, SiTypescript]} />
             </div>
         </section>
     )
