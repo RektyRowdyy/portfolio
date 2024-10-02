954a8e8c5aaab1ef56b7bb8a494747dc7af1a360
RektyRowdyy <rektyrowdy@gmail.com>
2024-10-02 21:33:46 +0530

    optimized projectsData in MyProjects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,5 @@
-import { SiAngular, SiExpress, SiMongodb, SiReact, SiTypescript } from "react-icons/si";
 import MyProjects from "./MyProjects";
-import { DiVisualstudio } from "react-icons/di";
-import { TbFileTypeSql } from "react-icons/tb";
-import { FaNodeJs, FaReact } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
-import { RiTailwindCssFill } from "react-icons/ri";
-
+import { projectsData } from "../../contants.js";
 
 export default function Projects() {
     return (
@@ -15,26 +9,17 @@ export default function Projects() {
                 <h1 className="text-2xl mt-24 font-semibold font-mono dark:text-white">PROJECTS</h1>
             </div>
             <div className="grid grid-cols-4 items-center justify-items-center">
-                <MyProjects title="INTELLILEARN LMS" year="2024"
-                    imgUrls={[`IntelliLearn/1.png`, `IntelliLearn/2.png`, `IntelliLearn/3.png`, `IntelliLearn/4.png`]}
-                    link="https://github.com/RektyRowdyy/IntelliLearn-LMS" icons={[SiAngular, SiTypescript, DiVisualstudio, TbFileTypeSql]} />
-                <MyProjects title="OTAKUGRAM" year="2024"
-                    imgUrls={[`Otakugram/1.png`, `Otakugram/2.png`]}
-                    link="https://github.com/RektyRowdyy/Otakugram" icons={[SiReact, FaNodeJs, SiExpress, SiMongodb]} />
-                <MyProjects title="YOUTUBE BACKEND" year="2024"
-                    imgUrls={[`Youtube-Backend/1.png`, `Youtube-Backend/2.png`, `Youtube-Backend/3.png`]}
-                    link="https://github.com/RektyRowdyy/Youtube-Backend" icons={[FaNodeJs, SiExpress, SiMongodb]} />
-                <MyProjects title="FREECODECAMP-AUTH" year="2024"
-                    imgUrls={[`FreeCodeCamp-AUTH/1.png`, `FreeCodeCamp-AUTH/2.png`, `FreeCodeCamp-AUTH/3.png`, `FreeCodeCamp-AUTH/4.png`]}
-                    link="https://github.com/RektyRowdyy/freecodecamp-AUTH" icons={[SiMongodb, SiExpress, FaReact, FaNodeJs]} />
-                <MyProjects title="HRIDYA-PORTFOLIO" year="2024"
-                    imgUrls={[`Portfolio/1.png`,`Portfolio/2.png`,`Portfolio/3.png`,`Portfolio/4.png`,`Portfolio/5.png` ]}
-                    link="https://github.com/RektyRowdyy/Chess" icons={[FaReact, IoLogoJavascript, RiTailwindCssFill]} />
-                <MyProjects title="CHESS-CLONE (in Progress)" year="2024"
-                    imgUrls={[`Chess-Clone/1.png`, `Chess-Clone/2.png`]}
-                    link="https://github.com/RektyRowdyy/Chess" icons={[SiMongodb, SiExpress, FaReact, FaNodeJs, SiTypescript]} />
+                {projectsData.map((project, index) => (
+                    <MyProjects
+                        key={index}
+                        title={project.title}
+                        year={project.year}
+                        imgUrls={project.imgUrls}
+                        link={project.link}
+                        icons={project.icons}
+                    />
+                ))}
             </div>
         </section>
     )
 }
-
---
+++ src/contants.js
@@ -0,0 +1,56 @@
+import { DiVisualstudio } from "react-icons/di";
+import { TbFileTypeSql } from "react-icons/tb";
+import { FaNodeJs, FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiAngular, SiExpress, SiMongodb, SiReact, SiTypescript } from "react-icons/si";
+
+export const projectsData = [
+        {
+            title: "INTELLILEARN LMS",
+            year: "2024",
+            imgUrls: [
+                "IntelliLearn/1.png",
+                "IntelliLearn/2.png",
+                "IntelliLearn/3.png",
+                "IntelliLearn/4.png"
+            ],
+            link: "https://github.com/RektyRowdyy/IntelliLearn-LMS",
+            icons: [SiAngular, SiTypescript, DiVisualstudio, TbFileTypeSql]
+        },
+        {
+            title: "OTAKUGRAM",
+            year: "2024",
+            imgUrls: ["Otakugram/1.png", "Otakugram/2.png"],
+            link: "https://github.com/RektyRowdyy/Otakugram",
+            icons: [SiReact, FaNodeJs, SiExpress, SiMongodb]
+        },
+        {
+            title: "YOUTUBE BACKEND",
+            year: "2024",
+            imgUrls: ["Youtube-Backend/1.png", "Youtube-Backend/2.png", "Youtube-Backend/3.png"],
+            link: "https://github.com/RektyRowdyy/Youtube-Backend",
+            icons: [FaNodeJs, SiExpress, SiMongodb]
+        },
+        {
+            title: "FREECODECAMP-AUTH",
+            year: "2024",
+            imgUrls: ["FreeCodeCamp-AUTH/1.png", "FreeCodeCamp-AUTH/2.png", "FreeCodeCamp-AUTH/3.png", "FreeCodeCamp-AUTH/4.png"],
+            link: "https://github.com/RektyRowdyy/freecodecamp-AUTH",
+            icons: [SiMongodb, SiExpress, FaReact, FaNodeJs]
+        },
+        {
+            title: "HRIDYA-PORTFOLIO",
+            year: "2024",
+            imgUrls: ["Portfolio/1.png", "Portfolio/2.png", "Portfolio/3.png", "Portfolio/4.png", "Portfolio/5.png"],
+            link: "https://github.com/RektyRowdyy/Chess",
+            icons: [FaReact, IoLogoJavascript, RiTailwindCssFill]
+        },
+        {
+            title: "CHESS-CLONE (in Progress)",
+            year: "2024",
+            imgUrls: ["Chess-Clone/1.png", "Chess-Clone/2.png"],
+            link: "https://github.com/RektyRowdyy/Chess",
+            icons: [SiMongodb, SiExpress, FaReact, FaNodeJs, SiTypescript]
+        }
+    ];
